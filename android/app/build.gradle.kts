572b0358619caf192d64b41e8dc5062db5a9a2bb
Your Name <youremail@example.com>
2026-08-20 21:38:56 +0530

    Sprint 4 - Lesson 4 complete and update Android build compatibility

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -30,7 +30,7 @@ android {
 
     defaultConfig {
         applicationId = "com.ifg.geniusacademy"
-        minSdk = 23
+        minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
